61ddfb6f8f7df8a7a12886a7f93f2c51cfd8b3fb
404Setup <153366651+404Setup@users.noreply.github.com>
2025-03-29 16:03:01 +0800

    25.03.2+1.18.2-fabric

--- src/main/java/one/tranic/pfc/ProfileCached.java
+++ src/main/java/one/tranic/pfc/ProfileCached.java
@@ -10,33 +10,9 @@ public class ProfileCached implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        /*if (!checkAuthenticationEnabled())
-            throw new UnsupportedAccountException();*/
-
         LOGGER.warn("ProfileCached is not compatible with non-OnlineMode accounts!");
         LOGGER.warn("(i.e. accounts that do not perform account verification, or accounts that use the third-party Yggdrasil verification server)");
         Config.reload();
         ProfileCachedCommand.register();
     }
-
-    /*private boolean checkAuthenticationEnabled() {
-        // Read the online-mode value from server.properties
-        try {
-            Path serverPropertiesPath = FabricLoader.getInstance().getGameDir().resolve("server.properties");
-            Properties properties = new Properties();
-
-            if (serverPropertiesPath.toFile().exists()) {
-                try (InputStream input = Files.newInputStream(serverPropertiesPath)) {
-                    properties.load(input);
-                }
-                return Boolean.parseBoolean(properties.getProperty("online-mode", "true"));
-            } else {
-                LOGGER.warn("server.properties file not found.");
-            }
-        } catch (IOException e) {
-            LOGGER.error("Failed to read server.properties file.", e);
-        }
-
-        return false;
-    }*/
 }
