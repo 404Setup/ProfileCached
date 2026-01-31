e55dbdeff06cd749e20834a6bf0e2f764b482bf4
404Setup <153366651+404Setup@users.noreply.github.com>
2026-01-31 19:30:13 +0800

    1.21.11

---
+++ build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    id("fabric-loom") version "1.13-SNAPSHOT"
+    id("maven-publish")
+}
+
+version = project.property("mod_version") as String
+group = project.property("maven_group") as String
+
+base {
+    archivesName.set(project.property("archives_base_name") as String)
+}
+
+val targetJavaVersion = 21
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
+    withSourcesJar()
+}
+
+repositories {
+    maven("https://jitpack.io")
+    maven("https://maven.terraformersmc.com/")
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${project.property("minecraft_version")}")
+    mappings(loom.officialMojangMappings())
+    modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")}")
+
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_version")}")
+
+    modImplementation(include("com.github.Carleslc.Simple-YAML:Simple-Yaml:1.8.4")!!)
+    modImplementation(include("com.github.ben-manes.caffeine:caffeine:3.2.0")!!)
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+    inputs.property("minecraft_version", project.property("minecraft_version"))
+    inputs.property("loader_version", project.property("loader_version"))
+    filteringCharset = "UTF-8"
+
+    filesMatching("fabric.mod.json") {
+        expand(
+            "version" to project.version as String,
+            "minecraft_version" to project.property("minecraft_version") as String,
+            "loader_version" to project.property("loader_version") as String
+        )
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.release.set(targetJavaVersion)
+}
+
+tasks.jar {
+    from("LICENSE") {
+        rename { "${it}_${project.base.archivesName}" }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = project.property("archives_base_name") as String
+            from(components["java"])
+        }
+    }
+
+    repositories {
+    }
+}
--- src/main/java/one/tranic/pfc/command/ProfileCachedCommand.java
+++ src/main/java/one/tranic/pfc/command/ProfileCachedCommand.java
@@ -4,10 +4,10 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.command.CommandSource;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.Text;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.permissions.Permissions;
 import one.tranic.pfc.config.Config;
 
 @SuppressWarnings("unused")
@@ -18,73 +18,77 @@ public class ProfileCachedCommand {
         });
     }
 
-    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        var rootCommand = CommandManager.literal("pfc")
-                .requires((source) -> source.hasPermissionLevel(3));
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+        var rootCommand = Commands.literal("pfc")
+                .requires((source) -> source.permissions().hasPermission(Permissions.COMMANDS_ADMIN));
 
-        rootCommand.then(CommandManager.literal("reload")
+        rootCommand.then(Commands.literal("reload")
                 .executes((ctx) -> reload(ctx.getSource()))
         );
 
 
-        rootCommand.then(CommandManager.literal("cleanall")
+        rootCommand.then(Commands.literal("cleanall")
                 .executes(ProfileCachedCommand::clean)
         );
 
-        rootCommand.then(CommandManager.literal("clean")
-                .then(CommandManager.argument("player", StringArgumentType.word())
-                        .suggests((ctx, builder) -> CommandSource.suggestMatching(ctx.getSource().getServer().getPlayerNames(), builder))
-                        .executes((ctx) -> cleanPlayer(ctx, StringArgumentType.getString(ctx, "player"))))
-        );
 
-        rootCommand.then(CommandManager.literal("size").executes(ProfileCachedCommand::size));
+        rootCommand.then(Commands.literal("clean")
+                .then(Commands.argument("player", StringArgumentType.word())
+                        .suggests((ctx, builder) -> {
+                            for (String playerName : ctx.getSource().getServer().getPlayerNames())
+                                builder.suggest(playerName);
+                            return builder.buildFuture();
+                        }).executes((ctx) -> cleanPlayer(ctx, StringArgumentType.getString(ctx, "player"))))
+        );
+        rootCommand.then(Commands.literal("size").executes(ProfileCachedCommand::size));
 
         dispatcher.register(rootCommand);
     }
 
-    private static int reload(ServerCommandSource source) {
+    private static int reload(CommandSourceStack source) {
         Config.reload();
 
-        source.sendFeedback(() -> Text.of("Profile Cached is reloaded!"), true);
+
+        source.sendSuccess(() -> Component.literal("Profile Cached is reloaded!"), true);
         return 1;
     }
 
-    private static int clean(CommandContext<ServerCommandSource> ctx) {
+    private static int clean(CommandContext<CommandSourceStack> ctx) {
         var source = ctx.getSource();
         var cache = Config.getCachedMain().cache();
         if (cache == null) {
-            source.sendFeedback(() -> Text.of("Profile Cached is not enabled!"), false);
+            source.sendFailure(Component.literal("Profile Cached is not enabled!"));
             return 0;
         }
         cache.invalidateAll();
-        source.sendFeedback(() -> Text.of("All caches have been cleaned!"), true);
+        source.sendSuccess(() -> Component.literal("All caches have been cleaned!"), true);
         return 1;
     }
 
-    private static int cleanPlayer(CommandContext<ServerCommandSource> ctx, String player) {
+    private static int cleanPlayer(CommandContext<CommandSourceStack> ctx, String player) {
         var source = ctx.getSource();
         if (Config.getCachedMain().cache() == null) {
-            source.sendFeedback(() -> Text.of("Profile Cached is not enabled!"), false);
+            source.sendFailure(Component.literal("Profile Cached is not enabled!"));
             return 0;
         }
         var result = Config.getCachedMain().cache().getIfPresent(player);
         if (result == null) {
-            source.sendFeedback(() -> Text.of("Player " + player + " is not cached!"), true);
+            source.sendFailure(Component.literal("Player " + player + " is not cached!"));
             return 0;
         }
         Config.getCachedMain().cache().invalidate(player);
-        source.sendFeedback(() -> Text.of("Cache for player " + player + " has been cleaned!"), true);
+        source.sendSuccess(() -> Component.literal("Cache for player " + player + " has been cleaned!"), true);
         return 1;
     }
 
-    private static int size(CommandContext<ServerCommandSource> ctx) {
+    private static int size(CommandContext<CommandSourceStack> ctx) {
         var source = ctx.getSource();
         var cache = Config.getCachedMain().cache();
         if (cache == null) {
-            source.sendFeedback(() -> Text.of("Profile Cached is not enabled!"), false);
+            source.sendFailure(Component.literal("Profile Cached is not enabled!"));
             return 0;
         }
-        source.sendFeedback(() -> Text.of("Profile Cached Size: " + cache.estimatedSize()), true);
+        source.sendSuccess(() -> Component.literal("Profile Cached Size: " + cache.estimatedSize()), true);
         return 1;
     }
 }
